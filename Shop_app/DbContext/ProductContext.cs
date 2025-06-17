25402bcb2c041f4b4b7d72a35c05e26efc0fc6c0
Mario <mario@gmail.com>
2025-06-17 17:33:38 +0300

    Add project files.

---
+++ Shop_app/DbContext/ProductContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop_app.Models;
+
+public class ProductContext : DbContext
+{
+    public ProductContext(DbContextOptions options) : base(options)
+    {
+    }
+    DbSet<Product> Products { get; set; }
+}
