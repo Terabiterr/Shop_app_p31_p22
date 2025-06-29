65a15bfd295a8aea6e715e265fd59b5b01df1d5e
Mario <mario@gmail.com>
2025-06-29 19:06:50 +0300

    added form register

---
+++ Shop_app/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_app.Controllers
+{
+    public class UserController : Controller
+    {
+        //http://localhost:port/user/register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
